a18e6e689e197ae63fc086490faab2d84058340e
Diego Weinmann <diego.weinmann@hotmail.com>
2020-10-29 23:53:51 -0300

    feat: add redux store and userEventsReducer

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core'
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<CSSReset />
-			<App />
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<CSSReset />
+				<App />
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+import { combineReducers, createStore } from 'redux'
+import userEventsReducer from './user-events'
+
+const rootReducer = combineReducers({
+  userEvents: userEventsReducer
+})
+
+const store = createStore(rootReducer)
+
+
+export default store
+
