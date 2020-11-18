414f8d04c33846bf06baadb312fbeb30c054ad4a
Diego Weinmann <diego.weinmann@hotmail.com>
2020-11-17 23:21:05 -0300

    feat: recorder component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Recorder from './components/Recorder'
 
 function App() {
-	return (
-		<div>
-			<h1>React App</h1>
-		</div>
-	)
+  return (
+    <div>
+      <h1>React App</h1>
+      <Recorder />
+    </div>
+  )
 }
 
 export default App
